413eac04cdaa17709824a60557db7e3ed18d0b65
JonathanSCasper <jcasper@harding.edu>
2021-01-29 02:01:26 -0600

    Added Nodes
    Added Nodes that count up the number of resources to be distributed.
    
    Currently two nodes begin as if owned by Player 1, and thus is awarded resources based on adjacent tiles, which can be viewed as a log in the Console.
    
    Pressing the 'Update Board' button will reset the nodes to blank, and therefore no resources will be awarded.
    
    Next to work on:
    1) determining if the maximum node count has been reached, and if so, do not distribute resources.
    2) Got the 'dots' that indicate maximum node count to show up on tiles.
    3) Add to score and make placeholder text to show resources and points.
    4) Add branches.
    5) Add ability to click on an empty node to place a new node.

--- Assets/Scripts/Core/GameBoard.cs
+++ Assets/Scripts/Core/GameBoard.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class GameBoard : MonoBehaviour
 {
 
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer TileRenderer;
+    private SpriteRenderer NodeRenderer;
+    private Color Orange = new Color(1f, 0.5f, 0f, 1f);
+    private Color Purple = new Color(0.5f, 0f, 0.5f, 1f);
 
     public GameObject tile1;
     public GameObject tile2;
@@ -20,10 +23,48 @@ public class GameBoard : MonoBehaviour
     public GameObject tile12;
     public GameObject tile13;
 
-    public List<tile> Gameboard = new List<tile>();
-    public List<GameObject> TileList = new List<GameObject>();
-    public List<GameObject> DotList = new List<GameObject>();
+    public GameObject Node1;
+    public GameObject Node2;
+    public GameObject Node3;
+    public GameObject Node4;
+    public GameObject Node5;
+    public GameObject Node6;
+    public GameObject Node7;
+    public GameObject Node8;
+    public GameObject Node9;
+    public GameObject Node10;
+    public GameObject Node11;
+    public GameObject Node12;
+    public GameObject Node13;
+    public GameObject Node14;
+    public GameObject Node15;
+    public GameObject Node16;
+    public GameObject Node17;
+    public GameObject Node18;
+    public GameObject Node19;
+    public GameObject Node20;
+    public GameObject Node21;
+    public GameObject Node22;
+    public GameObject Node23;
+    public GameObject Node24;
+
+    player Player1 = new player();
+    player Player2 = new player();
+    List<tile> Gameboard = new List<tile>();
+    List<GameObject> TileObjects = new List<GameObject>();
+    List<node> Nodes = new List<node>();
+    List<GameObject> NodeObjects = new List<GameObject>();
+    List<GameObject> DotList = new List<GameObject>();
     public string AiCode = "";
+
+    public class player
+    {
+        public int score = 0;
+        public int red = 0;
+        public int green = 0;
+        public int yellow = 0;
+        public int blue = 0;
+    }
 
     public class tile
     {
@@ -33,6 +74,8 @@ public class GameBoard : MonoBehaviour
         public bool one;
         public bool two;
         public bool three;
+        public bool owned = false;
+        public bool isBlocked = false;
 
         public tile(Color newColor, int newDots, string newCode, bool newOne, bool newTwo, bool newThree)
         {
@@ -44,34 +87,294 @@ public class GameBoard : MonoBehaviour
             three = newThree;
         }
     }
-    private void Awake()
+
+    public class node
     {
-        TileList.Add(tile1);
-        TileList.Add(tile2);
-        TileList.Add(tile3);
-        TileList.Add(tile4);
-        TileList.Add(tile5);
-        TileList.Add(tile6);
-        TileList.Add(tile7);
-        TileList.Add(tile8);
-        TileList.Add(tile9);
-        TileList.Add(tile10);
-        TileList.Add(tile11);
-        TileList.Add(tile12);
-        TileList.Add(tile13);
+        public int player = 0;
+        public tile tile1;
+        public tile tile2;
+        public tile tile3;
+        public tile tile4;
+
+        public node(tile newTile1, tile newTile2, tile newTile3, tile newTile4)
+        {
+            tile1 = newTile1;
+            tile2 = newTile2;
+            tile3 = newTile3;
+            tile4 = newTile4;
+        }
+    }
+
+    void Awake()
+    {
+        TileObjects.Add(tile1);
+        TileObjects.Add(tile2);
+        TileObjects.Add(tile3);
+        TileObjects.Add(tile4);
+        TileObjects.Add(tile5);
+        TileObjects.Add(tile6);
+        TileObjects.Add(tile7);
+        TileObjects.Add(tile8);
+        TileObjects.Add(tile9);
+        TileObjects.Add(tile10);
+        TileObjects.Add(tile11);
+        TileObjects.Add(tile12);
+        TileObjects.Add(tile13);
+
+        NodeObjects.Add(Node1);
+        NodeObjects.Add(Node2);
+        NodeObjects.Add(Node3);
+        NodeObjects.Add(Node4);
+        NodeObjects.Add(Node5);
+        NodeObjects.Add(Node6);
+        NodeObjects.Add(Node7);
+        NodeObjects.Add(Node8);
+        NodeObjects.Add(Node9);
+        NodeObjects.Add(Node10);
+        NodeObjects.Add(Node11);
+        NodeObjects.Add(Node12);
+        NodeObjects.Add(Node13);
+        NodeObjects.Add(Node14);
+        NodeObjects.Add(Node15);
+        NodeObjects.Add(Node16);
+        NodeObjects.Add(Node17);
+        NodeObjects.Add(Node18);
+        NodeObjects.Add(Node19);
+        NodeObjects.Add(Node20);
+        NodeObjects.Add(Node21);
+        NodeObjects.Add(Node22);
+        NodeObjects.Add(Node23);
+        NodeObjects.Add(Node24);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         SetUpBoard();
+        Nodes[3].player = 1;
+        Nodes[4].player = 1;
+        CheckNodes();       
     }
 
-    public void SetUpBoard()
+    void CheckNodes()
     {
-        Gameboard.Clear();
-        AiCode = "";
+        for(int i = 0; i < 24; i++)
+        {
+            if(Nodes[i].tile1 != null)
+            {
+                if(Nodes[i].player == 1)
+                {
+                    if(!Nodes[i].tile1.isBlocked)
+                    {
+                        if(Nodes[i].tile1.color == Color.red)
+                        {
+                            Player1.red += 1;
+                        }
+                        else if (Nodes[i].tile1.color == Color.green)
+                        {
+                            Player1.green += 1;
+                        }
+                        else if (Nodes[i].tile1.color == Color.yellow)
+                        {
+                            Player1.yellow += 1;
+                        }
+                        else if (Nodes[i].tile1.color == Color.blue)
+                        {
+                            Player1.blue += 1;
+                        }
+                    }
+                }
+                else if (Nodes[i].player == 2)
+                {
+                    if (!Nodes[i].tile1.isBlocked)
+                    {
+                        if (Nodes[i].tile1.color == Color.red)
+                        {
+                            Player2.red += 1;
+                        }
+                        else if (Nodes[i].tile1.color == Color.green)
+                        {
+                            Player2.green += 1;
+                        }
+                        else if (Nodes[i].tile1.color == Color.yellow)
+                        {
+                            Player2.yellow += 1;
+                        }
+                        else if (Nodes[i].tile1.color == Color.blue)
+                        {
+                            Player2.blue += 1;
+                        }
+                    }
+                }
+            }
+            if(Nodes[i].tile2 != null)
+            {
+                if(Nodes[i].player == 1)
+                {
+                    if (!Nodes[i].tile2.isBlocked)
+                    {
+                        if (Nodes[i].tile2.color == Color.red)
+                        {
+                            Player1.red += 1;
+                        }
+                        else if (Nodes[i].tile2.color == Color.green)
+                        {
+                            Player1.green += 1;
+                        }
+                        else if (Nodes[i].tile2.color == Color.yellow)
+                        {
+                            Player1.yellow += 1;
+                        }
+                        else if (Nodes[i].tile2.color == Color.blue)
+                        {
+                            Player1.blue += 1;
+                        }
+                    }
+                }
+                else if (Nodes[i].player == 2)
+                {
+                    if (!Nodes[i].tile2.isBlocked)
+                    {
+                        if (Nodes[i].tile2.color == Color.red)
+                        {
+                            Player2.red += 1;
+                        }
+                        else if (Nodes[i].tile2.color == Color.green)
+                        {
+                            Player2.green += 1;
+                        }
+                        else if (Nodes[i].tile2.color == Color.yellow)
+                        {
+                            Player2.yellow += 1;
+                        }
+                        else if (Nodes[i].tile2.color == Color.blue)
+                        {
+                            Player2.blue += 1;
+                        }
+                    }
+                }
+            }
+            if(Nodes[i].tile3 != null)
+            {
+                if (Nodes[i].player == 1)
+                {
+                    if (!Nodes[i].tile3.isBlocked)
+                    {
+                        if (Nodes[i].tile3.color == Color.red)
+                        {
+                            Player1.red += 1;
+                        }
+                        else if (Nodes[i].tile3.color == Color.green)
+                        {
+                            Player1.green += 1;
+                        }
+                        else if (Nodes[i].tile3.color == Color.yellow)
+                        {
+                            Player1.yellow += 1;
+                        }
+                        else if (Nodes[i].tile3.color == Color.blue)
+                        {
+                            Player1.blue += 1;
+                        }
+                    }
+                }
+                else if (Nodes[i].player == 2)
+                {
+                    if (!Nodes[i].tile3.isBlocked)
+                    {
+                        if (Nodes[i].tile3.color == Color.red)
+                        {
+                            Player2.red += 1;
+                        }
+                        else if (Nodes[i].tile3.color == Color.green)
+                        {
+                            Player2.green += 1;
+                        }
+                        else if (Nodes[i].tile3.color == Color.yellow)
+                        {
+                            Player2.yellow += 1;
+                        }
+                        else if (Nodes[i].tile3.color == Color.blue)
+                        {
+                            Player2.blue += 1;
+                        }
+                    }
+                }
+            }
+            if(Nodes[i].tile4 != null)
+            {
+                if (Nodes[i].player == 1)
+                {
+                    if (!Nodes[i].tile4.isBlocked)
+                    {
+                        if (Nodes[i].tile4.color == Color.red)
+                        {
+                            Player1.red += 1;
+                        }
+                        else if (Nodes[i].tile4.color == Color.green)
+                        {
+                            Player1.green += 1;
+                        }
+                        else if (Nodes[i].tile4.color == Color.yellow)
+                        {
+                            Player1.yellow += 1;
+                        }
+                        else if (Nodes[i].tile4.color == Color.blue)
+                        {
+                            Player1.blue += 1;
+                        }
+                    }
+                }
+                else if (Nodes[i].player == 2)
+                {
+                    if (!Nodes[i].tile4.isBlocked)
+                    {
+                        if (Nodes[i].tile4.color == Color.red)
+                        {
+                            Player2.red += 1;
+                        }
+                        else if (Nodes[i].tile4.color == Color.green)
+                        {
+                            Player2.green += 1;
+                        }
+                        else if (Nodes[i].tile4.color == Color.yellow)
+                        {
+                            Player2.yellow += 1;
+                        }
+                        else if (Nodes[i].tile4.color == Color.blue)
+                        {
+                            Player2.blue += 1;
+                        }
+                    }
+                }
+            }
+        }
 
+        for (int i = 0; i < 24; i++)
+        {
+            NodeRenderer = NodeObjects[i].GetComponent<SpriteRenderer>();
+            if (Nodes[i].player == 1)
+            {
+                NodeRenderer.color = Orange;
+            }
+            else if (Nodes[i].player == 2)
+            {
+                NodeRenderer.color = Purple;
+            }
+            else
+            {
+                NodeRenderer.color = Color.gray;
+            }
+        }
+
+        Debug.Log("Score: " + Player1.score);
+        Debug.Log("Red: " + Player1.red);
+        Debug.Log("Green: " + Player1.green);
+        Debug.Log("Yellow: " + Player1.yellow);
+        Debug.Log("Blue: " + Player1.blue);
+    }
+    void SetUpBoard()
+    {
         Gameboard.Add(new tile(Color.red, 1, "R1", true, false, false));
         Gameboard.Add(new tile(Color.red, 2, "R2", false, true, false));
         Gameboard.Add(new tile(Color.red, 3, "R3", false, false, true));
@@ -87,32 +390,43 @@ public class GameBoard : MonoBehaviour
         Gameboard.Add(new tile(Color.gray, 0, "XX", false, false, false));
 
         Gameboard = RandomizeBoard(Gameboard);
-
+        SetUpNodes();
         GenerateCode();
-        Debug.Log(AiCode);
 
         for (int i = 0; i < 13; i++)
         {
-            spriteRenderer = TileList[i].GetComponent<SpriteRenderer>();
-            spriteRenderer.color = Gameboard[i].color;
-
-            if(Gameboard[i].one)
-            {
-
-            }
-            else if(Gameboard[i].two)
-            {
-                
-            }
-            else if (Gameboard[i].three)
-            {
-
-            }
-            else
-            {
-
-            }
+            TileRenderer = TileObjects[i].GetComponent<SpriteRenderer>();
+            TileRenderer.color = Gameboard[i].color;
         }
+    }
+    
+    void SetUpNodes()
+    {
+        Nodes.Add(new node(null, null, null, Gameboard[0]));
+        Nodes.Add(new node(null, null, Gameboard[0], null));
+        Nodes.Add(new node(null, null, null, Gameboard[1]));
+        Nodes.Add(new node(null, Gameboard[0], Gameboard[1], Gameboard[2]));
+        Nodes.Add(new node(Gameboard[0], null, Gameboard[2], Gameboard[3]));
+        Nodes.Add(new node(null, null, Gameboard[3], null));
+        Nodes.Add(new node(null, null, null, Gameboard[4]));
+        Nodes.Add(new node(null, Gameboard[1], Gameboard[4], Gameboard[5]));
+        Nodes.Add(new node(Gameboard[1], Gameboard[2], Gameboard[5], Gameboard[6]));
+        Nodes.Add(new node(Gameboard[2], Gameboard[3], Gameboard[6], Gameboard[7]));
+        Nodes.Add(new node(Gameboard[3], null, Gameboard[7], Gameboard[8]));
+        Nodes.Add(new node(null, null, Gameboard[8], null));
+        Nodes.Add(new node(null, Gameboard[4], null, null));
+        Nodes.Add(new node(Gameboard[4], Gameboard[5], null, Gameboard[9]));
+        Nodes.Add(new node(Gameboard[5], Gameboard[6], Gameboard[9], Gameboard[10]));
+        Nodes.Add(new node(Gameboard[6], Gameboard[7], Gameboard[10], Gameboard[11]));
+        Nodes.Add(new node(Gameboard[7], Gameboard[8], Gameboard[11], null));
+        Nodes.Add(new node(Gameboard[8], null, null, null));
+        Nodes.Add(new node(null, Gameboard[9], null, null));
+        Nodes.Add(new node(Gameboard[9], Gameboard[10], null, Gameboard[12]));
+        Nodes.Add(new node(Gameboard[10], Gameboard[11], Gameboard[12], null));
+        Nodes.Add(new node(Gameboard[11], null, null, null));
+        Nodes.Add(new node(null, Gameboard[12], null, null));
+        Nodes.Add(new node(Gameboard[12], null, null, null));
+
     }
 
     // Update is called once per frame
@@ -121,7 +435,7 @@ public class GameBoard : MonoBehaviour
 
     }
 
-    private void GenerateCode()
+    void GenerateCode()
     {
         for(int i = 0; i < 13; i++)
         {
@@ -143,5 +457,36 @@ public class GameBoard : MonoBehaviour
         }
 
         return newGameboard;
+    }
+
+    public void UpdateBoard()
+    {
+        Player1 = ResetPlayer(Player1);
+        Player2 = ResetPlayer(Player2);
+        Nodes.Clear();
+        SetUpNodes();
+        CheckNodes();
+    }
+
+    player ResetPlayer(player Player)
+    {
+        Player.score = 0;
+        Player.red = 0;
+        Player.green = 0;
+        Player.yellow = 0;
+        Player.blue = 0;
+
+        return Player;
+    }
+    public void ResetGame()
+    {
+        Gameboard.Clear();
+        Nodes.Clear();
+        AiCode = "";
+
+        Player1 = ResetPlayer(Player1);
+        Player2 = ResetPlayer(Player2);
+
+        Start();
     }
 }
--- Assets/Scripts/Core/Node.cs
+++ Assets/Scripts/Core/Node.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class Node : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-
     // Start is called before the first frame update
     void Start()
     {
