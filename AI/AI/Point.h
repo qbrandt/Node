2250e3d140650c0c6023beaacddc16fdc5dda7ae
qbrandt <qbrandt@users.noreply.github.com>
2021-02-13 18:41:53 -0600

    Point Update

--- AI/AI/Point.h
+++ AI/AI/Point.h
@@ -6,7 +6,13 @@ public:
 	int Col;
 	
 	Point();
+	Point(int row, int col);
 
-	static Point TileNumberToPoint(int number);
-	static int PointToTileNumber(Point point);
+	static Point GetTileCoordinate(int id);
+	static Point GetNodeCoordinate(int id);
+	static Point GetBranchCoordinate(int id);
+private:
+	static Point Tiles[];
+	static Point Nodes[];
+	static Point Branches[];
 };
