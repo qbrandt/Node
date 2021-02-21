1424ab790e270c242f9fd2b058049ce74e90e460
Quinn Brandt <qbrandt@harding.edu>
2021-02-21 12:46:28 -0600

    Fixed Menu Buttons

--- Assets/Scripts/UI/Menu2.cs
+++ Assets/Scripts/UI/Menu2.cs
@@ -54,13 +54,11 @@ public class Menu2 : MonoBehaviour
                 SettingsPanel.SetActive(true);
                 break;
             case MenuScreen.QUIT:
+                MainPanel.SetActive(true);
                 // save any game data here
 #if UNITY_EDITOR
-         // Application.Quit() does not work in the editor so
-         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-         UnityEditor.EditorApplication.isPlaying = false;
 #else
-                Application.Quit();
+                Application.Quit(); 
 #endif
                 break;
         }
