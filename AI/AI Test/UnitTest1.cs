c127de72d8c6853615ed8901bfe4a5c563f05385
Quinn Brandt <qbrandt@harding.edu>
2021-02-22 22:40:02 -0600

    New unit test

--- AI/AI Test/UnitTest1.cs
+++ AI/AI Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace AI_Test
 {
@@ -20,6 +21,19 @@ namespace AI_Test
             Assert.AreNotEqual(firstMove,"X00");
             Assert.AreEqual(firstMove[0], 'N');
             Assert.AreEqual(firstMove[3], 'B');
+            //Assert.AreEqual(AI.GetMove("X00"), "X00");
+            //Assert.AreNotEqual(AI.GetMove("X00"), "X00");
+        }
+
+        [Test]
+        public void OpeningSecondMove()
+        {
+            AI.GameSetup("R1Y2B2G1G3XXY3G2B1Y1B3R2R3", false, false);
+            string firstMove = AI.GetMove("N00B00");
+            Assert.AreNotEqual(firstMove, "X00");
+            Assert.AreEqual(firstMove[0], 'N');
+            Assert.AreEqual(firstMove[3], 'B');
+            Console.WriteLine(firstMove);
             //Assert.AreEqual(AI.GetMove("X00"), "X00");
             //Assert.AreNotEqual(AI.GetMove("X00"), "X00");
         }
