6477fe86cce669a0d2cbc06dc660f7a29479654d
JakeOsborneHU <josborne3@harding.edu>
2021-03-24 10:35:04 -0500

    Singleplayer Settings Menu

---
+++ Assets/Scripts/UI/GameSettingsButtonBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettingsButtonBehaviour : MonoBehaviour
+{
+    public void FarmerSelection()
+    {
+        // disable all selection highlight game objects
+        // enable selection highlight object for pressed button
+        // store farmer value of the pressed button
+    }
+    
+    public void BackButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main 2.0");
+    }
+
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameBoard");
+    }
+}
