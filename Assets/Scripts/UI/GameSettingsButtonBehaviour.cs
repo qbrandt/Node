352b11f07e4d9e492159852462f5f10da1b8d4ff
JakeOsborneHU <josborne3@harding.edu>
2021-03-25 19:39:12 -0500

    single player game settings menu fully functional

--- Assets/Scripts/UI/GameSettingsButtonBehaviour.cs
+++ Assets/Scripts/UI/GameSettingsButtonBehaviour.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum Farmer
+{
+    BAIRD = 0,
+    FOUST= 1,
+    RAGSDALE = 2,
+    STEIL = 3
+}
 
 public class GameSettingsButtonBehaviour : MonoBehaviour
 {
-    public void FarmerSelection()
-    {
-        // disable all selection highlight game objects
-        // enable selection highlight object for pressed button
-        // store farmer value of the pressed button
-    }
-    
+    public static Farmer farmer;
+    public InputField usernameInput;
+    public static string username;
+    public Toggle goesFirstInput;
+    public static bool goesFirst;
+    public Toggle simpleAIInput;
+    public static bool simpleAI;
+        
     public void BackButton()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Menu 2.0");
@@ -18,6 +28,26 @@ public class GameSettingsButtonBehaviour : MonoBehaviour
 
     public void PlayButton()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("GameBoard");
+        if (username == "") // no username
+        {
+            // produce error message!
+        }
+        else // valid input
+        {
+            var farmerRadioToggles = GameObject.FindGameObjectsWithTag("Character Radio Toggle");
+            for (int i = 0; i < farmerRadioToggles.Length; ++i)
+            {
+                if (farmerRadioToggles[i].GetComponent<Toggle>().isOn)
+                {
+                    farmer = (Farmer)i;
+                }
+            }
+            username = usernameInput.text;
+            if (goesFirstInput.isOn) goesFirst = true;
+            else goesFirst = false;
+            if (simpleAIInput.isOn) simpleAI = true;
+            else simpleAI = false;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameBoard");
+        }
     }
 }
