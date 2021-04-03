1fd3a3cb4db0553af56b6e7bfdd1c5ed9df2eac0
sjhuff1 <77806020+sjhuff1@users.noreply.github.com>
2021-04-02 22:24:51 -0500

    RPCs Switched to Cached Events

---
+++ Assets/Scripts/Networking/CharacterSelection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField]
+    private ExitGames.Client.Photon.Hashtable _myCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
+
+}
