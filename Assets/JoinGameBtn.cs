7c973c5bc72cdcec5e4f4ee2415aa007e20b28be
awalls456 <71470628+awalls456@users.noreply.github.com>
2021-04-14 11:51:21 -0500

    merge from sprint1

--- Assets/JoinGameBtn.cs
+++ Assets/JoinGameBtn.cs
@@ -23,6 +23,7 @@ public class JoinGameBtn : MonoBehaviour
     public void Onclick_JoinRoom()
     {
         //PhotonNetwork.LeaveLobby();
+        PhotonNetwork.JoinLobby();
         MultiplayerPanel.SetActive(false);
         JoinRoomPanel.SetActive(true);
     }
